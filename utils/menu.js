49cb0332c41a79858b80818a87a23088957959d9
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-05-20 10:30:43 -0500

    Added the dark mode feature

--- utils/menu.js
+++ utils/menu.js
@@ -6,8 +6,33 @@ openMenuButton.addEventListener("click", () => {
 })
 
 
-
 const closeMenuButton = document.querySelector(".close-menu")
 closeMenuButton.addEventListener("click", () => {
     menuOpened.style.display = "none" 
 })
+
+
+const darkModeOffButton = document.querySelector(".dark-mode-off")
+const darkModeOnButton = document.querySelector(".dark-mode-on")
+darkModeOffButton.addEventListener("click", () => {
+
+    darkModeOnButton.style.display = "block"
+    darkModeOffButton.style.display = "none"
+
+    document.querySelectorAll("*").forEach(element => {
+        element.classList.add("dark-mode")
+    })
+})
+
+
+darkModeOnButton.addEventListener("click", () => {
+
+    darkModeOffButton.style.display = "block"
+    darkModeOnButton.style.display = "none"
+
+    document.querySelectorAll("*").forEach(element => {
+        element.classList.remove("dark-mode")
+    })
+})
+
+
